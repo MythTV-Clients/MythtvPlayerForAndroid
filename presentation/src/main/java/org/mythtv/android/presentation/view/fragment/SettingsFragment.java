1e5163853cea94ff7866e45101f3d390221f8483
Daniel Frey <dmfrey@gmail.com>
2016-01-27 15:32:53 -0500

    switched preferences to Android Support Preferences Compat Library

--- presentation/src/main/java/org/mythtv/android/presentation/view/fragment/SettingsFragment.java
+++ presentation/src/main/java/org/mythtv/android/presentation/view/fragment/SettingsFragment.java
@@ -21,11 +21,11 @@ package org.mythtv.android.presentation.view.fragment;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 import android.os.Bundle;
-import android.preference.CheckBoxPreference;
-import android.preference.EditTextPreference;
-import android.preference.Preference;
-import android.preference.PreferenceFragment;
-import android.preference.SwitchPreference;
+import android.support.v14.preference.SwitchPreference;
+import android.support.v7.preference.CheckBoxPreference;
+import android.support.v7.preference.EditTextPreference;
+import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceFragmentCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -36,22 +36,28 @@ import org.mythtv.android.domain.SettingsKeys;
 /*
  * Created by dmfrey on 4/7/15.
  */
-public class SettingsFragment extends PreferenceFragment implements OnSharedPreferenceChangeListener {
+public class SettingsFragment extends PreferenceFragmentCompat {
 
     private EditTextPreference mBackendUrl;
     private CheckBoxPreference internalPlayer;
     private SwitchPreference externalPlayerOverride;
 
     @Override
-    public void onCreate( Bundle savedInstanceState ) {
-        super.onCreate( savedInstanceState );
+    public void onCreatePreferences( Bundle bundle, String s ) {
 
         addPreferencesFromResource( R.xml.preferences );
 
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public void onCreate( Bundle savedInstanceState ) {
+        super.onCreate( savedInstanceState );
+
+
+    }
+
+    @Override
+    public View onCreateView( LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState ) {
 
         mBackendUrl = (EditTextPreference) getPreferenceManager().findPreference( SettingsKeys.KEY_PREF_BACKEND_URL );
         mBackendUrl.setOnPreferenceChangeListener( new Preference.OnPreferenceChangeListener() {
@@ -100,47 +106,6 @@ public class SettingsFragment extends PreferenceFragment implements OnSharedPref
         });
 
         return super.onCreateView( inflater, container, savedInstanceState );
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-
-        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener( this );
-
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-
-        getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener( this );
-
-    }
-
-    @Override
-    public void onSharedPreferenceChanged( SharedPreferences sharedPreferences, String key ) {
-
-//        if( key.equals( SettingsKeys.KEY_PREF_BACKEND_URL ) || key.equals( SettingsKeys.KEY_PREF_BACKEND_PORT ) ) {
-//
-//            MainApplication.getInstance().resetBackend();
-//
-//        }
-
-//        if( key.equals( MainApplication.KEY_PREF_INTERNAL_PLAYER ) ) {
-//            Log.i( TAG, "onSharedPreferenceChanged : key=" + key + ", value=" + sharedPreferences.getBoolean( MainApplication.KEY_PREF_INTERNAL_PLAYER, true ) );
-//            Log.i( TAG, "onSharedPreferenceChanged : key=" + MainApplication.KEY_PREF_EXTERNAL_PLAYER_OVERRIDE_VIDEO + ", value=" + sharedPreferences.getBoolean( MainApplication.KEY_PREF_EXTERNAL_PLAYER_OVERRIDE_VIDEO, true ) );
-//
-//            boolean internalVideoEnabled = sharedPreferences.getBoolean( MainApplication.KEY_PREF_INTERNAL_PLAYER, true );
-//            if( !internalVideoEnabled ) {
-//
-//                SwitchPreference external = (SwitchPreference) getPreferenceManager().findPreference( MainApplication.KEY_PREF_EXTERNAL_PLAYER_OVERRIDE_VIDEO );
-//                external.setChecked(false);
-//
-//            }
-//
-//        }
-
     }
 
 }
