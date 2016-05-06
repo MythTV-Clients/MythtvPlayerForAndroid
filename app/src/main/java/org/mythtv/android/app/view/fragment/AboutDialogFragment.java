5ebcf6fcb78af7bc1e4dbc1aa4dc4553f8d6dd51
Daniel Frey <dmfrey@gmail.com>
2016-05-05 21:59:57 -0400

    changes #148
    added branch, commit, and tag

--- app/src/main/java/org/mythtv/android/app/view/fragment/AboutDialogFragment.java
+++ app/src/main/java/org/mythtv/android/app/view/fragment/AboutDialogFragment.java
@@ -2,14 +2,12 @@ package org.mythtv.android.app.view.fragment;
 
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
-import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 
 import org.mythtv.android.app.R;
 
