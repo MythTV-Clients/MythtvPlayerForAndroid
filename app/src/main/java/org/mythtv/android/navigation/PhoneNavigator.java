3bd7bdd1bffcdf7e1931928d64e999eb3c2aa85b
Daniel Frey <dmfrey@gmail.com>
2016-06-12 09:13:32 -0400

    Refactor app, presentation and tv into app (#182)
    * Refactor app, presentation and tv into app
    
    This change requires support for Android SDKs 14, 15, 16
    be dropped. This is to accommodate not only building a
    single apk for phones, tablets and android tv, but also the
    forthcoming integrate of the latest exoplayer.
    
    fixes #180

---
+++ app/src/main/java/org/mythtv/android/navigation/PhoneNavigator.java
@@ -0,0 +1,219 @@
+/*
+ * MythtvPlayerForAndroid. An application for Android users to play MythTV Recordings and Videos
+ * Copyright (c) 2016. Daniel Frey
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.mythtv.android.navigation;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.util.Log;
+
+import org.joda.time.DateTime;
+import org.mythtv.android.view.activity.phone.MainPhoneActivity;
+import org.mythtv.android.view.activity.phone.PlayerActivity;
+import org.mythtv.android.view.activity.phone.ProgramDetailsActivity;
+import org.mythtv.android.view.activity.phone.ProgramDetailsSettingsActivity;
+import org.mythtv.android.view.activity.phone.ProgramListActivity;
+import org.mythtv.android.view.activity.phone.SettingsActivity;
+import org.mythtv.android.view.activity.phone.TitleInfoListActivity;
+import org.mythtv.android.view.activity.phone.VideoDetailsActivity;
+import org.mythtv.android.view.activity.phone.VideoDetailsSettingsActivity;
+import org.mythtv.android.view.activity.phone.VideoListActivity;
+import org.mythtv.android.view.activity.phone.VideoSeriesListActivity;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+/**
+ * Class used to navigate through the application.
+ *
+ * Created by dmfrey on 8/30/15.
+ */
+@Singleton
+public class PhoneNavigator {
+
+    private static final String TAG = PhoneNavigator.class.getSimpleName();
+
+    @Inject
+    public PhoneNavigator() {
+        //empty
+    }
+
+    public void navigateToHome( Context context ) {
+        Log.d( TAG, "navigateToHome : enter" );
+
+        if( null != context ) {
+            Log.d( TAG, "navigateToHome : context != null" );
+
+            Intent intentToLaunch = MainPhoneActivity.getCallingIntent( context );
+            context.startActivity( intentToLaunch );
+
+        }
+
+        Log.d( TAG, "navigateToHome : exit" );
+    }
+
+    public void navigateToTitleInfos( Context context ) {
+        Log.d( TAG, "navigateToTitleInfos : enter" );
+
+        if( null != context ) {
+            Log.d( TAG, "navigateToTitleInfos : context != null" );
+
+            context.startActivity( TitleInfoListActivity.getCallingIntent( context ) );
+
+        }
+
+        Log.d( TAG, "navigateToTitleInfos : exit" );
+    }
+
+    public void navigateToPrograms( Context context, boolean descending, int startIndex, int count, String titleRegEx, String recGroup, String storageGroup ) {
+        Log.d( TAG, "navigateToPrograms : enter" );
+
+        if( null != context ) {
+            Log.d( TAG, "navigateToPrograms : context != null" );
+
+            context.startActivity( ProgramListActivity.getCallingIntent( context, descending, startIndex, count, titleRegEx, recGroup, storageGroup ) );
+
+        }
+
+        Log.d( TAG, "navigateToPrograms : exit" );
+    }
+
+    public void navigateToProgram( Context context, int chanId, DateTime startTime ) {
+        Log.d( TAG, "navigateToProgram : enter" );
+
+        if( null != context ) {
+            Log.d( TAG, "navigateToProgram : context != null" );
+
+            context.startActivity( ProgramDetailsActivity.getCallingIntent( context, chanId, startTime ) );
+
+        }
+
+        Log.d( TAG, "navigateToProgram : exit" );
+    }
+
+    public void navigateToProgramSettings( Context context ) {
+        Log.d( TAG, "navigateToProgramSettings : enter" );
+
+        if( null != context ) {
+            Log.d( TAG, "navigateToProgramSettings : context != null" );
+
+            context.startActivity( ProgramDetailsSettingsActivity.getCallingIntent( context ) );
+
+        }
+
+        Log.d( TAG, "navigateToProgramSettings : exit" );
+    }
+
+    public void navigateToVideos( Context context ) {
+        Log.d( TAG, "navigateToVideos : enter" );
+
+        if( null != context ) {
+            Log.d( TAG, "navigateToVideos : context != null" );
+
+            context.startActivity( VideoListActivity.getCallingIntent( context ) );
+
+        }
+
+        Log.d( TAG, "navigateToVideos : exit" );
+    }
+
+    public void navigateToVideoSeries( Context context, String series ) {
+        Log.d( TAG, "navigateToVideoSeries : enter" );
+
+        if( null != context ) {
+            Log.d( TAG, "navigateToVideoSeries : context != null" );
+
+            context.startActivity( VideoSeriesListActivity.getCallingIntent( context, series ) );
+
+        }
+
+        Log.d( TAG, "navigateToVideoSeries : exit" );
+    }
+
+    public void navigateToVideo( Context context, int id, String storeageGroup, String filename, String hostname ) {
+        Log.d( TAG, "navigateToVideo : enter" );
+
+        if( null != context ) {
+            Log.d(TAG, "navigateToVideo : context != null");
+
+            context.startActivity( VideoDetailsActivity.getCallingIntent( context, id ) );
+
+        }
+
+        Log.d( TAG, "navigateToVideo : exit" );
+    }
+
+    public void navigateToVideoSettings( Context context ) {
+        Log.d( TAG, "navigateToVideoSettings : enter" );
+
+        if( null != context ) {
+            Log.d( TAG, "navigateToVideoSettings : context != null" );
+
+            context.startActivity( VideoDetailsSettingsActivity.getCallingIntent( context ) );
+
+        }
+
+        Log.d( TAG, "navigateToVideoSettings : exit" );
+    }
+
+    public void navigateToExternalPlayer( Context context, String uri ) {
+        Log.d( TAG, "navigateToExternalPlayer : enter" );
+
+        if( null != context ) {
+            Log.d( TAG, "navigateToExternalPlayer : context != null" );
+            Log.d( TAG, "navigateToExternalPlayer : uri=" + uri );
+
+            Intent intentToLaunch = new Intent( Intent.ACTION_VIEW );
+            intentToLaunch.setDataAndType( Uri.parse( uri ), "video/*" );
+            context.startActivity( intentToLaunch );
+
+        }
+
+        Log.d( TAG, "navigateToInternalPlayer : exit" );
+    }
+
+    public void navigateToVideoPlayer( Context context, String uri ) {
+        Log.d( TAG, "navigateToVideoPlayer : enter" );
+
+        if( null != context ) {
+            Log.d( TAG, "navigateToVideoPlayer : context != null" );
+            Log.d( TAG, "navigateToVideoPlayer : uri=" + uri );
+
+            context.startActivity( PlayerActivity.getCallingIntent( context, uri ) );
+
+        }
+
+        Log.d( TAG, "navigateToVideoPlayer : exit" );
+    }
+
+    public void navigateToSettings( Context context ) {
+        Log.d( TAG, "navigateToSettings : enter" );
+
+        if( null != context ) {
+            Log.d( TAG, "navigateToSettings : context != null" );
+
+            Intent intentToLaunch = SettingsActivity.getCallingIntent( context );
+            context.startActivity( intentToLaunch );
+
+        }
+
+        Log.d( TAG, "navigateToSettings : exit" );
+    }
+
+}
---
+++ app/src/main/java/org/mythtv/android/presenter/phone/UpcomingListPresenter.java
@@ -0,0 +1,169 @@
+/*
+ * MythtvPlayerForAndroid. An application for Android users to play MythTV Recordings and Videos
+ * Copyright (c) 2016. Daniel Frey
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.mythtv.android.presenter.phone;
+
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import org.mythtv.android.domain.Program;
+import org.mythtv.android.domain.exception.DefaultErrorBundle;
+import org.mythtv.android.domain.exception.ErrorBundle;
+import org.mythtv.android.domain.interactor.DefaultSubscriber;
+import org.mythtv.android.domain.interactor.UseCase;
+import org.mythtv.android.exception.ErrorMessageFactory;
+import org.mythtv.android.mapper.ProgramModelDataMapper;
+import org.mythtv.android.model.ProgramModel;
+import org.mythtv.android.view.ProgramListView;
+
+import java.util.Collection;
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+
+/**
+ * Created by dmfrey on 8/31/15.
+ */
+public class UpcomingListPresenter extends DefaultSubscriber<List<Program>> implements Presenter {
+
+    private static final String TAG = UpcomingListPresenter.class.getSimpleName();
+
+    private ProgramListView viewListView;
+
+    private final UseCase getUpcomingProgramListUseCase;
+    private final ProgramModelDataMapper programModelDataMapper;
+
+    @Inject
+    public UpcomingListPresenter( @Named( "upcomingProgramsList" ) UseCase getUpcomingProgramListUseCase, ProgramModelDataMapper programModelDataMapper ) {
+
+        this.getUpcomingProgramListUseCase = getUpcomingProgramListUseCase;
+        this.programModelDataMapper = programModelDataMapper;
+
+    }
+
+    public void setView( @NonNull ProgramListView view ) {
+        this.viewListView = view;
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void destroy() {
+
+        this.getUpcomingProgramListUseCase.unsubscribe();
+
+    }
+
+    /**
+     * Initializes the presenter by start retrieving the upcoming program list.
+     */
+    public void initialize() {
+
+        this.loadUpcomingProgramList();
+
+    }
+
+    /**
+     * Loads upcoming programs.
+     */
+    private void loadUpcomingProgramList() {
+
+        this.hideViewRetry();
+        this.showViewLoading();
+        this.getProgramList();
+
+    }
+
+    public void onProgramClicked( ProgramModel programModel ) {
+        Log.i( TAG, "onProgramClicked : programModel=" + programModel.toString() );
+
+        this.viewListView.viewProgram( programModel );
+
+    }
+
+    private void showViewLoading() {
+        this.viewListView.showLoading();
+    }
+
+    private void hideViewLoading() {
+        this.viewListView.hideLoading();
+    }
+
+    private void showViewRetry() {
+        this.viewListView.showRetry();
+    }
+
+    private void hideViewRetry() {
+        this.viewListView.hideRetry();
+    }
+
+    private void showErrorMessage( ErrorBundle errorBundle ) {
+
+        String errorMessage = ErrorMessageFactory.create( this.viewListView.getContext(), errorBundle.getException() );
+        this.viewListView.showError( errorMessage );
+
+    }
+
+    private void showProgramsCollectionInView( Collection<Program> programsCollection ) {
+
+        final Collection<ProgramModel> programModelsCollection = this.programModelDataMapper.transform( programsCollection );
+        this.viewListView.renderProgramList( programModelsCollection );
+
+    }
+
+    private void getProgramList() {
+
+        this.getUpcomingProgramListUseCase.execute( new ProgramListSubscriber() );
+
+    }
+
+    private final class ProgramListSubscriber extends DefaultSubscriber<List<Program>> {
+
+        @Override
+        public void onCompleted() {
+            UpcomingListPresenter.this.hideViewLoading();
+        }
+
+        @Override
+        public void onError( Throwable e ) {
+
+            UpcomingListPresenter.this.hideViewLoading();
+            UpcomingListPresenter.this.showErrorMessage( new DefaultErrorBundle( (Exception) e ) );
+            UpcomingListPresenter.this.showViewRetry();
+
+        }
+
+        @Override
+        public void onNext( List<Program> programs ) {
+
+            UpcomingListPresenter.this.showProgramsCollectionInView( programs );
+
+        }
+
+    }
+
+}
