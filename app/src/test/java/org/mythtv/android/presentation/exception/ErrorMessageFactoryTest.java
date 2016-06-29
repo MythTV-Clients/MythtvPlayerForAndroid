537881574921ed9b2a57e184a07901e4db764434
Daniel Frey <dmfrey@gmail.com>
2016-06-28 21:34:28 -0400

    Coveralls test
    changes #117

--- app/src/test/java/org/mythtv/android/presentation/exception/ErrorMessageFactoryTest.java
+++ app/src/test/java/org/mythtv/android/presentation/exception/ErrorMessageFactoryTest.java
@@ -1,9 +1,9 @@
 package org.mythtv.android.presentation.exception;
 
-import android.test.AndroidTestCase;
+import android.content.Context;
+import android.test.mock.MockContext;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.mythtv.android.data.exception.NetworkConnectionException;
 import org.mythtv.android.data.exception.ProgramNotFoundException;
@@ -13,19 +13,22 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-@Ignore
-public class ErrorMessageFactoryTest extends AndroidTestCase {
+public class ErrorMessageFactoryTest {
+
+    private Context context;
 
     @Before
     public void setUp() throws Exception {
-        super.setUp();
+
+        context = new MockContext();
+
     }
 
     @Test
     public void testNetworkConnectionErrorMessage() {
 
-        String expectedMessage = getContext().getString( R.string.exception_message_no_connection );
-        String actualMessage = ErrorMessageFactory.create( getContext(), new NetworkConnectionException() );
+        String expectedMessage = context.getString( R.string.exception_message_no_connection );
+        String actualMessage = ErrorMessageFactory.create( context, new NetworkConnectionException() );
 
         assertThat( actualMessage, is( equalTo( expectedMessage ) ) );
 
@@ -34,8 +37,8 @@ public class ErrorMessageFactoryTest extends AndroidTestCase {
     @Test
     public void testProgramNotFoundErrorMessage() {
 
-        String expectedMessage = getContext().getString(R.string.exception_message_program_not_found);
-        String actualMessage = ErrorMessageFactory.create( getContext(), new ProgramNotFoundException() );
+        String expectedMessage = context.getString(R.string.exception_message_program_not_found);
+        String actualMessage = ErrorMessageFactory.create( context, new ProgramNotFoundException() );
 
         assertThat( actualMessage, is( equalTo( expectedMessage ) ) );
 
