5d33b11cd3622d4c5f88dc1cdf6d287cbeaffb5c
Daniel Frey <dmfrey@gmail.com>
2017-01-22 10:21:56 -0500

    Merge remote-tracking branch 'origin/develop' into bugfix/issue_186
    # Conflicts:
    #	app/src/main/java/org/mythtv/android/presentation/view/activity/tv/SearchableActivity.java

--- app/src/main/java/org/mythtv/android/presentation/view/activity/tv/SearchableActivity.java
+++ app/src/main/java/org/mythtv/android/presentation/view/activity/tv/SearchableActivity.java
@@ -23,7 +23,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.SearchRecentSuggestions;
-import android.support.v17.leanback.widget.SpeechRecognitionCallback;
 import android.util.Log;
 
 import org.mythtv.android.R;
