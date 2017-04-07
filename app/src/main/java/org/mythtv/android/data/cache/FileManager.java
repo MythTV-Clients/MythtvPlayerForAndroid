b9a8b259df08878303fdd799619116c6ca5a8b93
Daniel Frey <dmfrey@gmail.com>
2017-04-07 12:57:20 -0400

    Close resource with 'try with resources' with correct charset
    FindBugs

--- app/src/main/java/org/mythtv/android/data/cache/FileManager.java
+++ app/src/main/java/org/mythtv/android/data/cache/FileManager.java
@@ -33,10 +33,15 @@ import android.util.Log;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.nio.charset.StandardCharsets;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
@@ -65,10 +70,11 @@ public class FileManager {
 
         if( !file.exists() ) {
 
-            try( FileWriter writer = new FileWriter( file ) ) {
+            try(
+                    OutputStreamWriter writer = new OutputStreamWriter( new FileOutputStream( file ), StandardCharsets.UTF_8 )
+            ) {
 
                 writer.write( fileContent );
-                writer.close();
 
             } catch( FileNotFoundException e ) {
 
@@ -98,15 +104,15 @@ public class FileManager {
         if( file.exists() ) {
 
             String stringLine;
-            try {
+            try(
+                    InputStream in = new FileInputStream( file );
+                    Reader reader = new InputStreamReader(in, StandardCharsets.UTF_8);
+                    BufferedReader bufferedReader = new BufferedReader( reader )
+            ) {
 
-                FileReader fileReader = new FileReader( file );
-                BufferedReader bufferedReader = new BufferedReader( fileReader );
                 while( ( stringLine = bufferedReader.readLine() ) != null ) {
                     fileContentBuilder.append(stringLine).append('\n');
                 }
-                bufferedReader.close();
-                fileReader.close();
 
             } catch( FileNotFoundException e ) {
 
