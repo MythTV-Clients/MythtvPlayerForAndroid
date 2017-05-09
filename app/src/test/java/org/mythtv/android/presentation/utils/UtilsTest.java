3c21180fe72eb8b3c80d5453a4e1d1e83f662088
Daniel Frey <dmfrey@gmail.com>
2017-05-08 21:52:31 -0400

    Add version check that includes git commit sha
    closes #240

--- app/src/test/java/org/mythtv/android/presentation/utils/UtilsTest.java
+++ app/src/test/java/org/mythtv/android/presentation/utils/UtilsTest.java
@@ -2,8 +2,7 @@ package org.mythtv.android.presentation.utils;
 
 import org.junit.Test;
 
-import static org.hamcrest.Matchers.equalTo;
-import static org.junit.Assert.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  *
@@ -18,25 +17,27 @@ public class UtilsTest {
     @Test
     public void testMeetsMinimumVersion() throws Exception {
 
-        assertThat( Utils.meetsMinimumVersion( "b6ae90c071", 0.28f ), equalTo( false ) );
-        assertThat( Utils.meetsMinimumVersion( "Unknown", 0.28f ), equalTo( false ) );
-        assertThat( Utils.meetsMinimumVersion( "v0.27.6", 0.28f ), equalTo( false ) );
-        assertThat( Utils.meetsMinimumVersion( "v0.27", 0.28f ), equalTo( false ) );
-        assertThat( Utils.meetsMinimumVersion( "0.27", 0.28f ), equalTo( false ) );
-        assertThat( Utils.meetsMinimumVersion( "v0.27-xyz", 0.28f ), equalTo( false ) );
-        assertThat( Utils.meetsMinimumVersion( "v0.26", 0.28f ), equalTo( false ) );
-        assertThat( Utils.meetsMinimumVersion( "0.26", 0.28f ), equalTo( false ) );
-        assertThat( Utils.meetsMinimumVersion( "v0.26-xyz", 0.28f ), equalTo( false ) );
+        assertThat( Utils.meetsMinimumVersion( "b6ae90c071", 0.28f ) ).isEqualTo( false );
+        assertThat( Utils.meetsMinimumVersion( "Unknown", 0.28f ) ).isEqualTo( false );
+        assertThat( Utils.meetsMinimumVersion( "v0.27.6", 0.28f ) ).isEqualTo( false );
+        assertThat( Utils.meetsMinimumVersion( "v0.27", 0.28f ) ).isEqualTo( false );
+        assertThat( Utils.meetsMinimumVersion( "0.27", 0.28f ) ).isEqualTo( false );
+        assertThat( Utils.meetsMinimumVersion( "v0.27-xyz", 0.28f ) ).isEqualTo( false );
+        assertThat( Utils.meetsMinimumVersion( "v0.26", 0.28f ) ).isEqualTo( false );
+        assertThat( Utils.meetsMinimumVersion( "0.26", 0.28f ) ).isEqualTo( false );
+        assertThat( Utils.meetsMinimumVersion( "v0.26-xyz", 0.28f ) ).isEqualTo( false );
 
-        assertThat( Utils.meetsMinimumVersion( "v0.28", 0.28f ), equalTo( true ) );
-        assertThat( Utils.meetsMinimumVersion( "0.28", 0.28f ), equalTo( true ) );
-        assertThat( Utils.meetsMinimumVersion( "v0.28-xyz", 0.28f ), equalTo( true ) );
-        assertThat( Utils.meetsMinimumVersion( "v28.0", 0.28f ), equalTo( true ) );
+        assertThat( Utils.meetsMinimumVersion( "v0.28", 0.28f ) ).isEqualTo( true );
+        assertThat( Utils.meetsMinimumVersion( "0.28", 0.28f ) ).isEqualTo( true );
+        assertThat( Utils.meetsMinimumVersion( "v0.28-xyz", 0.28f ) ).isEqualTo( true );
+        assertThat( Utils.meetsMinimumVersion( "v28.0", 0.28f ) ).isEqualTo( true );
 
-        assertThat( Utils.meetsMinimumVersion( "v0.29", 0.28f ), equalTo( true ) );
-        assertThat( Utils.meetsMinimumVersion( "0.29", 0.28f ), equalTo( true ) );
-        assertThat( Utils.meetsMinimumVersion( "v0.29-xyz", 0.28f ), equalTo( true ) );
-        assertThat( Utils.meetsMinimumVersion( "v29.0", 0.28f ), equalTo( true ) );
+        assertThat( Utils.meetsMinimumVersion( "v0.29", 0.28f ) ).isEqualTo( true );
+        assertThat( Utils.meetsMinimumVersion( "0.29", 0.28f ) ).isEqualTo( true );
+        assertThat( Utils.meetsMinimumVersion( "v0.29-xyz", 0.28f ) ).isEqualTo( true );
+        assertThat( Utils.meetsMinimumVersion( "v29.0", 0.28f ) ).isEqualTo( true );
+
+        assertThat( Utils.meetsMinimumVersion( "v0.28.1-21-ge26a33c", 0.28f ) ).isEqualTo( true );
 
     }
 
